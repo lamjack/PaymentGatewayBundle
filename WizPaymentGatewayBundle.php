2834d29d6e9b1159bb3382d8b6021180de401d3f
JackLam <linjue@wilead.com>
2015-10-20 11:47:20 +0800

    no message

---
+++ WizPaymentGatewayBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Author: jack<linjue@wilead.com>
+ * Date: 15/10/20
+ */
+namespace Wiz\PaymentGateWayBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class WizPaymentGatewayBundle
+ * @package Wiz\PaymentGateWayBundle
+ */
+class WizPaymentGatewayBundle extends Bundle
+{
+}
