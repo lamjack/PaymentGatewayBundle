f8a5f0e3370086cfc267d400e9a0b995c4f4a656
JackLam <linjue@wilead.com>
2015-10-20 15:18:08 +0800

    no message

--- Common/AbstractGateway.php
+++ Common/AbstractGateway.php
@@ -6,9 +6,19 @@
 
 namespace Wiz\PaymentGatewayBundle\Common;
 
+/**
+ * Class AbstractGateway
+ * @package Wiz\PaymentGatewayBundle\Common
+ */
 abstract class AbstractGateway implements GatewayInterface
 {
+    /**
+     * Get the short name of the Gateway
+     *
+     * @return string
+     */
     public function getShortName()
     {
+        return Helper::getGatewayShortName(get_class($this));
     }
 }
